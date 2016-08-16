0104ad3c04b05f13b86ef6ebf474ae219d9f5563
jaddyASC <Jeffronaddy@gmail.com>
2016-08-16 14:24:43 -0400

    Zone-Website

---
+++ FinalProject/Reminders.js
@@ -0,0 +1,9 @@
+// var x=new Date();
+// x.setFullYear(2013,2,1);
+// var today = new Date();
+
+// if (x>today) {
+//     alert("Today is before March 1st, 2013");
+// } else {
+//     alert("Today is after March 1st, 2013");
+// }
